63eaf1ffd7aa2d735faf994087d0e82bc74a168c
sarahT04 <sarahtanujaya@gmail.com>
2022-03-19 11:12:19 +0700

    some fixes

--- soal_numbering.py
+++ soal_numbering.py
@@ -1,20 +1,48 @@
 
 from time import sleep
-import os 
 
-filename = os.path.dirname(os.path.realpath(__file__))  # Change your directory here.
+filename = r"C:\Users\hp\Documents\KODE Sarah"
 file_encode = '.txt'
 
 problems_dict = dict()
 
-problems_amount = int(input("How many problems?\n> "))
+
+problems_amount = int(input("Berapa banyak soal?\n> "))
 for num in range(1, problems_amount+1):
     problems_dict[num] = input(f"{num}. ").upper()
     
+
+def get_all_problems(func_dict: dict, row: int=6) -> str:
+    """_summary_
+
+    Args:
+        func_dict (dict): the dict full of numbers and answers
+        row (int, optional): Amount of rows. Defaults to 3.
+
+    Returns:
+        str: returns a row column of all answers.
+    """
+    keys = list(func_dict.keys())
+    cols = 5
+    s = ''
+    for i in range(1, cols + 1):
+        j = i
+        for c in range(1, row):
+            try:
+                s += str(j + 1) + '. '
+                s += str(func_dict[keys[j - 1]]).ljust(3)
+                j += cols
+            except IndexError as e:
+                print(str(e))
+                break
+        s += '\n' 
+    return (s)
+        
+
 while True:
     user_choice = input("Save, Input false problems, or look at answers?\n> ")
     if user_choice == '0':
-        save_as = input("With what name should it be saved?\n> ")
+        save_as = input("Simpan dengan nama apa?\n> ")
         new_filename = filename + '\\' + save_as + file_encode
         with open(new_filename, 'a') as f:
             for k, v in problems_dict.items():
@@ -34,13 +62,17 @@ while True:
                 print("ERROR: " + str(e))
         amount = len(problems_dict)
         calc = ((amount - wrongs)/amount) * 100
-        print(f"You got score: {calc}, {wrongs}/{amount} wrong answers.")
-        problems_dict['Score'] = f"{calc}, {wrongs}/{amount} wrong."
+        print(f"Kamu mendapatkan score: {calc}, {wrongs}/{amount} dari soal salah.")
+        problems_dict['Score'] = f"{calc}, {wrongs}/{amount} salah"
     elif user_choice == '2':
         while True:
-            num = input("Which number? Enter 'done' once you're done.\n> ")
+            num = input("Which number? Enter 'done' once you're done, or 'all' to see all numbers.\n> ")
             if num == 'done':
+                break
+            if num == 'all':
+                print(get_all_problems(problems_dict))
                 break
             print(num + '. ', problems_dict[int(num)])
     elif user_choice == 'done':
         sleep(300)
+        
