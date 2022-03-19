59c9fe52b3a03e0689823078db9fbed1cdc2fe1b
sarahT04 <sarahtanujaya@gmail.com>
2022-03-19 22:28:00 +0700

    finalized

--- soal_numbering.py
+++ soal_numbering.py
@@ -1,15 +1,46 @@
 
 from time import sleep
+import os 
 
-filename = r"C:\Users\hp\Documents\KODE Sarah"
+filename = os.path.dirname(os.path.realpath(__file__))
 file_encode = '.txt'
 
 problems_dict = dict()
+
 
 problems_amount = int(input("Berapa banyak soal?\n> "))
 for num in range(1, problems_amount+1):
     problems_dict[num] = input(f"{num}. ").upper()
     
+
+def get_all_problems(func_dict: dict, row: int=3) -> str:
+    """_summary_
+
+    Args:
+        func_dict (dict): the dict full of numbers and answers
+        row (int, optional): Amount of rows. Defaults to 3.
+
+    Returns:
+        str: returns a row column of all answers.
+    """
+    keys = list(func_dict.keys())
+    cols = round(len(func_dict) / row)
+    s = ''
+    for i in range(1, cols + 1):
+        j = i
+        for c in range(1, row + 1):
+            try:
+                p = func_dict[keys[j - 1]]
+                s += str(j) + '. '
+                s += str(p).ljust(3)
+                j += cols
+            except IndexError as e:
+                print(str(e))
+                break
+        s += '\n' 
+    return (s)
+        
+
 while True:
     user_choice = input("Save, Input false problems, or look at answers?\n> ")
     if user_choice == '0':
@@ -37,9 +68,13 @@ while True:
         problems_dict['Score'] = f"{calc}, {wrongs}/{amount} salah"
     elif user_choice == '2':
         while True:
-            num = input("Which number? Enter 'done' once you're done.\n> ")
+            num = input("Which number? Enter 'done' once you're done, or 'all' to see all numbers.\n> ")
             if num == 'done':
+                break
+            if num == 'all':
+                print(get_all_problems(problems_dict))
                 break
             print(num + '. ', problems_dict[int(num)])
     elif user_choice == 'done':
         sleep(300)
+        
