3b4fd652482501e276ab998786e22af53c7f814c
sarahT04 <sarahtanujaya@gmail.com>
2022-03-22 22:51:25 +0700

    commented, finalized

--- soal_numbering.py
+++ soal_numbering.py
@@ -8,6 +8,7 @@ file_encode = '.txt'
 problems_dict = dict()
 
 
+# Get user numbers
 problems_amount = int(input("Berapa banyak soal?\n> "))
 for num in range(1, problems_amount+1):
     problems_dict[num] = input(f"{num}. ").upper()
@@ -25,14 +26,15 @@ def get_all_problems(func_dict: dict, row: int=3) -> str:
     keys = list(func_dict.keys())
     cols = round(len(func_dict) / row)
     s = ''
-    for i in range(1, cols + 1):
-        j = i
-        for c in range(1, row + 1):
+    for c in range(1, cols + 1):
+        j = c
+        for r in range(1, row + 1):
             try:
-                p = func_dict[keys[j - 1]]
-                s += str(j) + '. '
-                s += str(p).ljust(6)
-                j += cols
+                d = func_dict[keys[j - 1]] # The data
+                if d != 'Score' : # If it's score, don't store it 
+                    s += str(j) + '. ' # To number
+                    s += str(d).ljust(6)
+                    j += cols
             except IndexError as e:
                 print(str(e))
                 break
@@ -42,30 +44,35 @@ def get_all_problems(func_dict: dict, row: int=3) -> str:
 
 while True:
     user_choice = input("Save, Input false problems, or look at answers?\n> ")
-    if user_choice == '0':
+    if user_choice == '0' or user_choice == 'save':
         save_as = input("Simpan dengan nama apa?\n> ")
+        if not save_as:
+            save_as = 'Untitled'
         new_filename = filename + '\\' + save_as + file_encode
         with open(new_filename, 'a') as f:
             for k, v in problems_dict.items():
-                f.write(f"{str(k)}. {v}\n")
+                if str(k) != 'Score':
+                    f.write(f"{str(k)}. {v}\n")
+                    continue
+                f.write(f"\n{k}: {v}")
         print('Saved')
-    elif user_choice == '1':
+    elif user_choice == '1' or user_choice == 'input':
         wrongs = 0
         while True:
-            false_problems = input("Enter number and correct answer [3, A]. Enter 'done' once you're done:\n> ")
-            if false_problems == 'done':
-                break
-            false_problems = false_problems.split(', ')
             try:
-                problems_dict[int(false_problems[0])] += f" x {false_problems[1].upper()}"
+                false_problems = input("Enter number and correct answer [3, A]. Enter 'done' once you're done:\n> ")
+                if false_problems == 'done':
+                    break
+                false_problems = false_problems.split(', ')
+                problems_dict[int(false_problems[0])] += f" x {false_problems[1].upper()}" # example: A x B
                 wrongs += 1
+                amount = len(problems_dict)
+                calc = ((amount - wrongs)/amount) * 100
+                print(f"Kamu mendapatkan score: {calc}, {wrongs}/{amount} dari soal salah.")
+                problems_dict['Score'] = f"{calc}, {wrongs}/{amount} salah" # Add to dictionary
             except Exception as e:
                 print("ERROR: " + str(e))
-        amount = len(problems_dict)
-        calc = ((amount - wrongs)/amount) * 100
-        print(f"Kamu mendapatkan score: {calc}, {wrongs}/{amount} dari soal salah.")
-        problems_dict['Score'] = f"{calc}, {wrongs}/{amount} salah"
-    elif user_choice == '2':
+    elif user_choice == '2' or user_choice == 'look':
         while True:
             num = input("Which number? Enter 'done' once you're done, or 'all' to see all numbers.\n> ")
             if num == 'done':
@@ -73,7 +80,11 @@ while True:
             if num == 'all':
                 print(get_all_problems(problems_dict))
                 break
-            print(num + '. ', problems_dict[int(num)])
+            try: # Means user inputted a number
+                print(num + '. ', problems_dict[int(num)])
+            except Exception as e:
+                print("ERROR: " + str(e))
     elif user_choice == 'done':
-        sleep(300)
+        print("Thank you. Program will close in 1 minute.")
+        sleep(60)
         
